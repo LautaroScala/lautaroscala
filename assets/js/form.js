4f48c489e32821e9bbf59934af3a0e9e295b9271
LautaroScala <scala.lautaro@hotmail.com>
2023-12-07 12:05:54 -0300

    [ADD] Ocultado url de contacto

---
+++ assets/js/form.js
@@ -0,0 +1,29 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    var formData = new FormData(event.target);
+
+    var email = document.getElementById("email").value;
+
+    // Validar formato de correo electrónico 
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      alert("Por favor, ingresa un correo electrónico válido.");
+      event.preventDefault();
+      return false;
+    }
+    fetch('https://formspree.io/f/mrgwraoq', {
+        method: 'POST',
+        body: formData,
+        headers: {
+            'Accept':'application/json'
+        }
+      })
+      .then(response => response.json())
+      .then(data => {
+        alert("Enviado con exito! Gracias por contactarme!")
+      })
+      .catch(error => {
+        alert(`${error}`)
+      })
+  });
